f08b7fc3b2c3300f6e196501029a85581139b300
salah3x <salah.loukili@gmail.com>
2018-11-03 23:21:30 +0000

    add firebase config for prod
    Signed-off-by: salah3x <salah.loukili@gmail.com>

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyD66mUCbsUEJPCH1XHFeUoozjFFvryJjNs',
+    authDomain: 'alfou-5b922.firebaseapp.com',
+    databaseURL: 'https://alfou-5b922.firebaseio.com',
+    projectId: 'alfou-5b922',
+    storageBucket: 'alfou-5b922.appspot.com',
+    messagingSenderId: '1056023747698'
+  }
 };
