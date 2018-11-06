192735366bbce516fca254cc7a049c37e4a94a38
salah3x <salah.loukili@gmail.com>
2018-11-06 20:59:30 +0000

    Add and configure sendgrid lib

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,6 @@
-let functions = require('firebase-functions');
+const functions = require('firebase-functions');
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey('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');
 
 /**
  * This endpoint get triggered whenever a new message arrived in the mail box.
@@ -20,3 +22,14 @@ exports.send = functions.firestore.document('/messages/{messageId}').onCreate((s
   console.log(context);
   response.send('OK');
 });
+
+/*
+const msg = {
+  from: 'salah@local.com',
+  to: 'salah.loukili@gmail.com',
+  subject: 'Sending with SendGrid is Fun',
+  text: 'and easy to do anywhere, even with Node.js',
+  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+};
+sgMail.send(msg);
+*/
