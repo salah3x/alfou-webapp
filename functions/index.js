69c5362402ea676f58f3ad7417b6ca6b15c0b99a
salah3x <salah.loukili@gmail.com>
2018-11-19 18:52:43 +0100

    finish onMessageArrived function

--- functions/index.js
+++ functions/index.js
@@ -2,9 +2,11 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
-const Parser = require('@sendgrid/inbound-mail-parser');
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey('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');
+
+const Busboy = require('busboy');
+const HTMLParser = require('node-html-parser');
 
 /**
  * This endpoint get triggered whenever a new message arrived in the mail box.
@@ -12,12 +14,50 @@ sgMail.setApiKey('SG.kkDGAhI8TiCa7OpG2WXdZg.PNhWilOfsFi-gzeVXFs3dsne2G6e4TmgL-gX
  * an ongoing conversation depending on the subject (if it contains the conversation number)
  */
 exports.onMessageArrived = functions.https.onRequest((request, response) => {
-  try{
-    console.log('Message received', new Parser({}, request.body).keyValues());
-  }catch(e) {
-    console.log(e);
-  }
-  response.send('OK');
+  const busboy = new Busboy({ headers: request.headers });
+  const email = {};
+  busboy.on('field', (fieldname, val) => {
+    if (fieldname === 'from')
+      email.name = val.split('"')[1];
+    if (fieldname === 'subject')
+      email.subject = val;
+    if (fieldname === 'envelope')
+      email.from = JSON.parse(val).from;
+    if (fieldname === 'html')
+      email.body = HTMLParser.parse(val).firstChild.toString();
+  });
+  busboy.on('finish', () => {
+    let p;
+    if (email.subject.includes('[ID#')) {
+      email.subject = ' ' + email.subject;
+      const id = email.subject.split('[ID#')[1].split(']')[0];
+      p = admin.firestore().collection(`/messages/${id}/replies`).add({
+        body: email.body,
+        date: new Date(),
+        me: false,
+      }).then(() => admin.firestore().collection('/messages').doc(id).update({new: true, done: false}));
+    } else {
+      p = admin.firestore().collection('/messages').add({
+        name: email.name,
+        from: email.from,
+        subject: email.subject,
+        body: email.body,
+        date: new Date(),
+        done: false,
+        new: true
+      });
+    }
+    p.then(() => {
+      console.log('Email received :', email);
+      response.end();
+      return;
+    }).catch(reason => {
+      console.error(reason);
+      response.end();
+      return;
+    });
+  });
+  busboy.end(request.body);
 });
 
 /**
@@ -25,7 +65,7 @@ exports.onMessageArrived = functions.https.onRequest((request, response) => {
  */
 exports.onNewEmail = functions.firestore.document('emails/{id}').onCreate((snapshot) => {
   const msg = {
-    from: {name: 'Alfou-sécurité', email:'info@async.zone'},
+    from: {name: 'Alfou-sécurité', email:'info@alfou.net'},
     to: snapshot.data().to,
     subject: snapshot.data().subject,
     html: snapshot.data().body.replace(/\n/g, '<br/>')
@@ -44,7 +84,7 @@ exports.onNewReply = functions.firestore.document('messages/{idM}/{repliesId}/{i
     return snapshot.ref.parent.parent.get()
       .then(value => {
         return {
-          from: {name: 'Alfou-sécurité', email:'info@async.zone'},
+          from: {name: 'Alfou-sécurité', email:'info@alfou.net'},
           to: value.data().from,
           subject: `[ID#${context.params.idM}]: ${value.data().subject}`,
           html: snapshot.data().body.replace(/\n/g, '<br/>')
@@ -65,4 +105,3 @@ exports.onNewReply = functions.firestore.document('messages/{idM}/{repliesId}/{i
   }
   return Promise.resolve('');
 });
-
